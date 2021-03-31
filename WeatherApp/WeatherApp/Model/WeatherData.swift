7e35db7a7eb24dfd60c7852e37e129dfda1b30f2
xingY97 <45300300+xingY97@users.noreply.github.com>
2021-03-31 19:33:39 -0400

    finished Networking

--- WeatherApp/WeatherApp/Model/WeatherData.swift
+++ WeatherApp/WeatherApp/Model/WeatherData.swift
@@ -9,11 +9,47 @@ import Foundation
 
 struct WeatherData {
     
-    let weatherURL = "http://api.openweathermap.org/data/2.5/forecast?id={2275099}&appid=9658f91424a20230f084ae61132c2327&&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/forecast?id={2275099}&appid=9658f91424a20230f084ae61132c2327&units=metric"
+    
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
     }
     
+    func performRequest(urlString: String) {
+        //Create a URL
+        if let url = URL(string: urlString) {
+            
+            //Create a URLsession
+            let session = URLSession(configuration: .default)
+            
+            //Give the session a task
+            let task = session.dataTask(with: url) { (data, response, error) in
+                //if there is no error
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                //If there are errors
+                if let safeData = data {
+                    self.parseJSON(weatherTempData: safeData)
+                }
+            }
+            
+            //Start the task
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherTempData: Data) {
+        let decoder = JSONDecoder()
+        do{
+            let decodedData = try decoder.decode(WeatherTempData.self, from: weatherTempData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
+        }
+    }
 }
---
+++ WeatherApp/WeatherApp/Model/WeatherTempData.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherTempData.swift
+//  WeatherApp
+//
+//  Created by X Y on 3/31/21.
+//
+
+//day name, weather icon, description, city
+import Foundation
+
+struct WeatherTempData: Decodable {
+    
+    let name: String
+    let main: Main
+    let weather: [Weather]
+    
+    
+    
+}
+
+struct Main: Decodable {
+    
+    let temp: Double
+}
+
+
+struct Weather: Decodable {
+    let description: String
+}
