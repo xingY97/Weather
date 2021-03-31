981201996f06d29efbf0e911fa109fc651c47935
xingY97 <45300300+xingY97@users.noreply.github.com>
2021-03-31 14:04:48 -0400

    basic UI setup

---
+++ WeatherApp/WeatherApp/WeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by X Y on 3/31/21.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
